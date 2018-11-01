8702229ba2e3304167e8e5d6868e966d509d36f2
Otavio Salvador <otavio@ossystems.com.br>
2018-11-01 11:30:38 -0300

    net: stmmac: dwmac-rk: Do not print error when PHY regulator is not found
    The PHY regulator is optional, so when it is not present move the message
    level from error to debug, which is more appropriate.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-rk.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-rk.c
@@ -1233,7 +1233,7 @@ static struct rk_priv_data *rk_gmac_setup(struct platform_device *pdev,
 			dev_err(dev, "phy regulator is not available yet, deferred probing\n");
 			return ERR_PTR(-EPROBE_DEFER);
 		}
-		dev_err(dev, "no regulator found\n");
+		dev_dbg(dev, "no regulator found\n");
 		bsp_priv->regulator = NULL;
 	}
 
