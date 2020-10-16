f9e06347dace5fd41106f076c632eee73f39eb2d
Otavio Salvador <otavio@ossystems.com.br>
2020-10-16 16:05:37 -0300

    media: coda: Change firmware probing order
    We move the firmware, from 'vpu' subdirectory as first place to look at,
    so it works fine when booting the device.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>
    (cherry picked from commit d4f3dcb65f2e93e3d2112feba5ff6d6f143e97be)

--- drivers/media/platform/chips-media/coda-common.c
+++ drivers/media/platform/chips-media/coda-common.c
@@ -3039,8 +3039,8 @@ enum coda_platform {
 static const struct coda_devtype coda_devdata[] = {
 	[CODA_IMX27] = {
 		.firmware     = {
-			"vpu_fw_imx27_TO2.bin",
 			"vpu/vpu_fw_imx27_TO2.bin",
+			"vpu_fw_imx27_TO2.bin",
 			"v4l-codadx6-imx27.bin"
 		},
 		.product      = CODA_DX6,
@@ -3053,8 +3053,8 @@ static const struct coda_devtype coda_devdata[] = {
 	},
 	[CODA_IMX51] = {
 		.firmware     = {
-			"vpu_fw_imx51.bin",
 			"vpu/vpu_fw_imx51.bin",
+			"vpu_fw_imx51.bin",
 			"v4l-codahx4-imx51.bin"
 		},
 		.product      = CODA_HX4,
@@ -3068,8 +3068,8 @@ static const struct coda_devtype coda_devdata[] = {
 	},
 	[CODA_IMX53] = {
 		.firmware     = {
-			"vpu_fw_imx53.bin",
 			"vpu/vpu_fw_imx53.bin",
+			"vpu_fw_imx53.bin",
 			"v4l-coda7541-imx53.bin"
 		},
 		.product      = CODA_7541,
@@ -3083,8 +3083,8 @@ static const struct coda_devtype coda_devdata[] = {
 	},
 	[CODA_IMX6Q] = {
 		.firmware     = {
-			"vpu_fw_imx6q.bin",
 			"vpu/vpu_fw_imx6q.bin",
+			"vpu_fw_imx6q.bin",
 			"v4l-coda960-imx6q.bin"
 		},
 		.product      = CODA_960,
@@ -3098,8 +3098,8 @@ static const struct coda_devtype coda_devdata[] = {
 	},
 	[CODA_IMX6DL] = {
 		.firmware     = {
-			"vpu_fw_imx6d.bin",
 			"vpu/vpu_fw_imx6d.bin",
+			"vpu_fw_imx6d.bin",
 			"v4l-coda960-imx6dl.bin"
 		},
 		.product      = CODA_960,
